825a71052e393fe016909d1e08859777f7c1392d
SerotoninTheft <87555495+SerotoninTheft@users.noreply.github.com>
2024-07-28 21:34:13 -0700

    Add files via upload

---
+++ arcotorio/prototypes/items.lua
@@ -0,0 +1,320 @@
+data:extend({
+    {
+        type = "item",
+        name = "Arco-bead",
+        icon = "__arcotorio__/graphics/Arco-bead.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Rock-Arco-bead",
+        icon = "__arcotorio__/graphics/Rock-Arco-bead.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Paper-Arco-bead",
+        icon = "__arcotorio__/graphics/Paper-Arco-bead.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Scissors-Arco-bead",
+        icon = "__arcotorio__/graphics/Scissors-Arco-bead.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Arco-orb",
+        icon = "__arcotorio__/graphics/Arco-orb.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Water-Arco-orb",
+        icon = "__arcotorio__/graphics/Water-Arco-orb.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Air-Arco-orb",
+        icon = "__arcotorio__/graphics/Air-Arco-orb.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Fire-Arco-orb",
+        icon = "__arcotorio__/graphics/Fire-Arco-orb.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Earth-Arco-orb",
+        icon = "__arcotorio__/graphics/Earth-Arco-orb.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Arco-boulder",
+        icon = "__arcotorio__/graphics/Arco-boulder.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Pride-Arco-boulder",
+        icon = "__arcotorio__/graphics/Pride-Arco-boulder.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Greed-Arco-boulder",
+        icon = "__arcotorio__/graphics/Greed-Arco-boulder.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Lust-Arco-boulder",
+        icon = "__arcotorio__/graphics/Lust-Arco-boulder.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Envy-Arco-boulder",
+        icon = "__arcotorio__/graphics/Envy-Arco-boulder.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Gluttony-Arco-boulder",
+        icon = "__arcotorio__/graphics/Gluttony-Arco-boulder.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Wrath-Arco-boulder",
+        icon = "__arcotorio__/graphics/Wrath-Arco-boulder.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Sloth-Arco-boulder",
+        icon = "__arcotorio__/graphics/Sloth-Arco-boulder.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Arco-planet",
+        icon = "__arcotorio__/graphics/Arco-planet.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Mercury-Arco-planet",
+        icon = "__arcotorio__/graphics/Mercury-Arco-planet.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Venus-Arco-planet",
+        icon = "__arcotorio__/graphics/Venus-Arco-planet.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Earth-Arco-planet",
+        icon = "__arcotorio__/graphics/Earth-Arco-planet.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Mars-Arco-planet",
+        icon = "__arcotorio__/graphics/Mars-Arco-planet.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Jupiter-Arco-planet",
+        icon = "__arcotorio__/graphics/Jupiter-Arco-planet.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Saturn-Arco-planet",
+        icon = "__arcotorio__/graphics/Saturn-Arco-planet.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Uranus-Arco-planet",
+        icon = "__arcotorio__/graphics/Uranus-Arco-planet.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Neptune-Arco-planet",
+        icon = "__arcotorio__/graphics/Neptune-Arco-planet.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "McArco-sphere",
+        icon = "__arcotorio__/graphics/McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Walmart-McArco-sphere",
+        icon = "__arcotorio__/graphics/Walmart-McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "McDonalds-McArco-sphere",
+        icon = "__arcotorio__/graphics/McDonalds-McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Tesco-McArco-sphere",
+        icon = "__arcotorio__/graphics/Tesco-McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Wendys-McArco-sphere",
+        icon = "__arcotorio__/graphics/Wendys-McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Subway-McArco-sphere",
+        icon = "__arcotorio__/graphics/Subway-McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "White-Castle-McArco-sphere",
+        icon = "__arcotorio__/graphics/White-Castle-McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Taco-Bell-McArco-sphere",
+        icon = "__arcotorio__/graphics/Taco-Bell-McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "KFC-McArco-sphere",
+        icon = "__arcotorio__/graphics/KFC-McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Jimmy-Johns-McArco-sphere",
+        icon = "__arcotorio__/graphics/Jimmy-Johns-McArco-sphere.png",
+        icon_size = 64,
+        subgroup = "intermediate-product",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "Arcofolder",
+        icon = "__arcotorio__/graphics/Arcofolder-icon.png",
+        icon_size = 64,
+        subgroup = "production-machine",
+        order = "a[Arcofolder]",
+        place_result = "Arcofolder",
+        stack_size = 50
+    },
+    {
+        type = "item",
+        name = "Advanced-Arcofolder",
+        icon = "__arcotorio__/graphics/advanced-arcofolder-icon.png",
+        icon_size = 64,
+        subgroup = "production-machine",
+        order = "a[Advanced arcofolder]",
+        place_result = "Advanced-Arcofolder",
+        stack_size = 50
+    },
+    {
+        type = "item",
+        name = "McArcofolder",
+        icon = "__arcotorio__/graphics/McArcofolder-icon.png",
+        icon_size = 64,
+        subgroup = "production-machine",
+        order = "a[McArcofolder]",
+        place_result = "McArcofolder",
+        stack_size = 50
+    },
+})
