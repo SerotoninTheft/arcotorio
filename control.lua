7bf2e7ed03822ad382161e83a2985381ab79808a
ArchEzekiel <66336623+ArchEzekiel@users.noreply.github.com>
2024-07-31 23:52:10 -0400

    starting items actually work

--- control.lua
+++ control.lua
@@ -1,13 +1,20 @@
-local util = require("util")
-
 script.on_event(defines.events.on_player_created, function(event)
     local player = game.get_player(event.player_index)
-
-    local start = function()
-        return {
-            ["Arco-bead"] = 20
-        }
+    if player and player.get_main_inventory() then 
+        player.get_main_inventory().insert({name="Arco-bead", count=15})
     end
+end)
 
-    util.insert_safe(player, start())
+script.on_event(defines.events.on_cutscene_finished, function(event)
+    local player = game.get_player(event.player_index)
+    if player and player.get_main_inventory() then 
+        player.get_main_inventory().insert({name="Arco-bead", count=15})
+    end
+end)
+
+script.on_event(defines.events.on_cutscene_cancelled, function(event)
+    local player = game.get_player(event.player_index)
+    if player and player.get_main_inventory() then 
+        player.get_main_inventory().insert({name="Arco-bead", count=15})
+    end
 end)
